1582302abf80d45c5bb9a4b19d55750205c96c77
fabriciocm11 <fabricio.macedo34@gmail.com>
2024-02-03 00:49:14 -0300

    result-js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function onclick (){
 
-const button = document.getElementById("Sortear")
-console.log("ola java") 
+const button = document.getElementById("Sortear") 
+}
+
+function valuesInput (){
+
+    const min = document.getElementById("input").value
+    const max = document.getElementById("input-two").value
+    const resultInput = document.getElementById("input-three")
+
+  
+    const calc = Math.floor(Math.random() * (max - min + 1)) - min;
+    resultInput.value = calc
+    
+    console.log(calc)
+    
 }
 
 
 
-
-
-
+document.addEventListener("change", valuesInput)
 document.addEventListener("click",onclick)
