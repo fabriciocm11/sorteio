1d683be4a9613fd86805e0bb364c43117a0c8167
fabriciocm11 <fabricio.macedo34@gmail.com>
2024-02-03 01:01:34 -0300

    finalizado

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ const button = document.getElementById("Sortear")
 
 function valuesInput (){
 
-    const min = document.getElementById("input").value
-    const max = document.getElementById("input-two").value
+    const min = Math.ceil(document.getElementById("input").value)
+    const max = Math.floor(document.getElementById("input-two").value)
     const resultInput = document.getElementById("input-three")
 
-  
-    const calc = Math.floor(Math.random() * (max - min + 1)) - min;
+    const calc = Math.floor(Math.random() * (max - min + 1)) + min;
     resultInput.value = calc
     
     console.log(calc)
